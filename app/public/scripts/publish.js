215d807ef3689a0ce0a64bc6ee36100599b95336
Andre <apcianflone@gmail.com>
2017-03-31 20:11:02 -0600

    file upload

--- app/public/scripts/publish.js
+++ app/public/scripts/publish.js
@@ -309,3 +309,64 @@ function printResult(msg){
   $('#ace0').append(newdiv1);
   newdiv1.delay(3000).fadeOut();
 }
+
+//-----------------------------------------------------------------------------
+// Upload file
+//-----------------------------------------------------------------------------
+// Variable to store your files
+var files;
+
+// Add events
+$('input[type=file]').on('change', prepareUpload);
+
+// Grab the files and set them to our variable
+function prepareUpload(event)
+{
+  files = event.target.files;
+}
+$('form').on('submit', uploadFiles);
+
+// Capture the form submit and upload the files
+function uploadFiles(event)
+{
+  event.stopPropagation(); // Stop stuff happening
+  event.preventDefault(); // Totally stop stuff happening
+
+    // START A LOADING SPINNER HERE
+
+    // Create a formdata object and add the files
+    var data = new FormData();
+    $.each(files, function(key, value)
+    {
+        data.append(key, value);
+    });
+
+    $.ajax({
+        url: 'submit.php?files',
+        type: 'POST',
+        data: data,
+        cache: false,
+        dataType: 'json',
+        processData: false, // Don't process the files
+        contentType: false, // Set content type to false as jQuery will tell the server its a query string request
+        success: function(data, textStatus, jqXHR)
+        {
+            if(typeof data.error === 'undefined')
+            {
+                // Success so call function to process the form
+                submitForm(event, data);
+            }
+            else
+            {
+                // Handle errors here
+                console.log('ERRORS: ' + data.error);
+            }
+        },
+        error: function(jqXHR, textStatus, errorThrown)
+        {
+            // Handle errors here
+            console.log('ERRORS: ' + textStatus);
+            // STOP LOADING SPINNER
+        }
+    });
+}
